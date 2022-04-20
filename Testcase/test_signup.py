94e48968778129edd42593acb3ee34ba2c42b285
Balaji.D <46712265+BalajiDhanaraj@users.noreply.github.com>
2022-04-20 12:35:29 +0530

    update

--- Testcase/test_signup.py
+++ Testcase/test_signup.py
@@ -8,12 +8,12 @@ from Utilities import dataProvider
 
 # log = Logger(__name__,logging.INFO)
 
-class Test_SignUp(BaseTest):
-
-    @pytest.mark.parametrize("name, phoneNum, email, country, city, username, password",
-                             dataProvider.get_data("LoginTest"))
-    def test_doSignUp(self, name, phoneNum, email, country, city, username, password):
-        # log.logger.info("Test Do Sign up started")
-        regPage = RegistrationPage(self.driver)
-        regPage.fillForm(name, phoneNum, email, country, city, username, password)
-        # log.logger.info("Test Do Sign up successfully executed")
+# class Test_SignUp(BaseTest):
+#
+#     @pytest.mark.parametrize("name, phoneNum, email, country, city, username, password",
+#                              dataProvider.get_data("LoginTest"))
+#     def test_doSignUp(self, name, phoneNum, email, country, city, username, password):
+#         # log.logger.info("Test Do Sign up started")
+#         regPage = RegistrationPage(self.driver)
+#         regPage.fillForm(name, phoneNum, email, country, city, username, password)
+#         # log.logger.info("Test Do Sign up successfully executed")
